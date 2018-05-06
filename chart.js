2e90dea46bc21f7cedca38364f9b6ade8bf5c014
melvinhardin <melvinhardin14071991@gmail.com>
2018-05-06 23:46:28 +0200

    Added all bosses and GID's
    Disabled chart for this push, will be changed back

--- chart.js
+++ chart.js
@@ -1,20 +1,200 @@
+var vgGID = [
+                991015174, //Mechanics
+                190462791, //Ren - DPS
+                811503596, //FB - DPS
+                838391600, //Condi BS
+                1058908524, //Power BS
+                1785579560, //Weaver - DPS
+                1657696023, //Slb - DPS
+                1339404060, //Scourge - DPS
+                1132006602, //Chrono - support
+                1992582571, //DH - DPS
+                1579402544, //Druid - support
+                999598709 //Holo - DPS
+              ];
+
+var gorseGID = [
+                1701108246, //Mechanics
+                2103724885, //Ren - DPS
+                971514289, //FB - DPS
+                1478042865, //Condi BS
+                949529543, //Power BS
+                942005734, //Weaver - DPS
+                1143826790, //Slb - DPS
+                2075644689, //Scourge - DPS
+                917443487, //Chrono - support
+                2063296291, //DH - DPS
+                134926887, //Druid - support
+                147886991 //Holo - DPS
+              ];
+
+var sabGID = [
+                985382956, //Mechanics
+                902829492, //Ren - DPS
+                1831565737, //FB - DPS
+                1781476171, //Condi BS
+                415691172, //Power BS
+                302290482, //Weaver - DPS
+                146341096, //Slb - DPS
+                1780621990, //Scourge - DPS
+                2031957583, //Chrono - support
+                310204702, //DH - DPS
+                1003900799, //Druid - support
+                2102220127, //Holo - DPS
+              ];
+
+var slothGID = [
+                1669870756, //Mechanics
+                1340294905, //Ren - DPS
+                1095908613, //FB - DPS
+                1364298249, //Condi BS
+                416934536, //Power BS
+                1283816465, //Weaver - DPS
+                1247044226, //Slb - DPS
+                1615895992, //Scourge - DPS
+                2110320791, //Chrono - support
+                442952749, //DH - DPS
+                1511154784, //Druid - support
+                675250952, //Holo - DPS
+              ];
+
+var matthiasGID = [
+                1235926549, //Mechanics
+                1899713201, //Ren - DPS
+                785982441, //FB - DPS
+                236547606, //Condi BS
+                233473682, //Power BS
+                807394279, //Weaver - DPS
+                1363984045, //Slb - DPS
+                1842123078, //Scourge - DPS
+                183521363, //Chrono - support
+                736887254, //DH - DPS
+                738926685, //Druid - support
+                592679401 //Holo - DPS
+              ];
+
+var kcGID = [
+                1047857245, //Mechanics
+                898429453, //Ren - DPS
+                1945768043, //FB - DPS
+                913543469, //Condi BS
+                214385073, //Power BS
+                181826106, //Weaver - DPS
+                1064131140, //Slb - DPS
+                409634735, //Scourge - DPS
+                447244827, //Chrono - support
+                568195480, //DH - DPS
+                1106663828, //Druid - support
+                1638416332 //Holo - DPS
+];
+
+var xeraGID = [ 
+                2042986611, //Mechanics
+                868093496, //Ren - DPS
+                1112758682, //FB - DPS
+                1425335353, //Condi BS
+                1495249342, //Power BS
+                711582192, //Weaver - DPS
+                1492374439, //Slb - DPS
+                1541183482, //Scourge - DPS
+                1710616243, //Chrono - support
+                66929659, //DH - DPS
+                31629366, //Druid - support
+                1611150169 //Holo - DPS
+
+];
 
 var cairnGID = [
                 1480282890, //Mechanics
                 516807299, //Ren - DPS
-                777978539, //Ren - DPS
-                119436615, //FB - DPS
-                858369185, //Condi BS
-                1015892548, //Power BS
-                940026070, //Weaver - DPS
+                777978539, //FB - DPS
+                119436615, //Condi BS
+                858369185, //Power BS
+                1015892548, //Weaver - DPS
+                940026070, //Slb - DPS
                 461179812, //Scourge - DPS
                 1494700677, //Chrono - support
                 683383792, //DH - DPS
-                491765656, //Druid - Support
+                491765656, //Druid - support
                 840142421 //Holo - DPS
-                ];
+              ];
 
-var bossSheet= [
+var moGID = [
+                809025824, //Mechanics
+                208587933, //Ren - DPS
+                913371253, //FB - DPS
+                454378217, //Condi BS
+                407633478, //Power BS
+                423931843, //Weaver - DPS
+                691809147, //Slb - DPS
+                1576480614, //Scourge - DPS
+                1144038318, //Chrono - support
+                2000255683, //DH - DPS
+                1154589258, //Druid - support
+                85044944, //Holo - DPS
+              ];
+
+var samGID = [
+                24437191, //Mechanics
+                1178893327, //Ren - DPS
+                432764259, //FB - DPS
+                1715174861, //Condi BS
+                1091748785, //Power BS
+                1494053032, //Weaver - DPS
+                1847580111, //Slb - DPS
+                1466349830, //Scourge - DPS
+                37001876, //Chrono - support
+                289013984, //DH - DPS
+                189665426, //Druid - support
+                156270862, //Holo - DPS
+              ];
+
+var deimGID = [
+                839279019, //Mechanics
+                1990120780, //Ren - DPS
+                1757068528, //FB - DPS
+                476256543, //Condi BS
+                1754608620, //Power BS
+                611302285, //Weaver - DPS
+                1549609737, //Slb - DPS
+                1828913427, //Scourge - DPS
+                32125949, //Chrono - DPS
+                1445569781, //DH - DPS
+                2074683547, //Druid - support
+                1145777690, //Holo - DPS
+              ];
+
+var shGID = [
+                20845267, //Mechanics
+                2046268883, //Ren - DPS
+                1263066701, //FB - DPS
+                1200644838, //Condi BS
+                691915459, //Power BS
+                994224862, //Weaver - DPS
+                1818820202, //Slb - DPS
+                1651543817, //Scourge - DPS
+                1042258015, //Chrono - support
+                511857208, //DH - DPS
+                558226147, //Druid - support
+                841973933, //Holo - DPS
+              ];
+
+var dhuumGID = [
+                2115074878, //Mechanics
+                1348320342, //Ren - DPS
+                701660904, //FB - DPS
+                1763519200, //Condi BS
+                1084207949, //Power BS
+                1950577508, //Weaver - DPS
+                1753352609, //Slb - DPS
+                35660111, //Scourge - DPS
+                1061457989, //Chrono - support
+                1511397589, //DH - DPS
+                1513342853, //Druid - support
+                765123966, //Holo - DPS
+              ];
+
+var bossSheet = [
                 'https://docs.google.com/spreadsheets/d/1L6Z8UZNQvgLi1Ve9ybmUjD6yLdDIz4D65c4FfQB3Sec/gviz/tq?gid=', //vg
                 'https://docs.google.com/spreadsheets/d/1GiN7hb_m_aWAV811YhSddN2d32B7QnqfR45_lBNSUhM/gviz/tq?gid=', //gorse
                 'https://docs.google.com/spreadsheets/d/1HAqrzdwqxxNTC4Q3nuU2salCq9Kpeuqp5axUZS4OR3A/gviz/tq?gid=', //sab
@@ -29,12 +209,17 @@ var bossSheet= [
                 'https://docs.google.com/spreadsheets/d/1vxyXvP2cZf-m-HsZCvY7Yv-rrqHGAvN66-KYmX4g3yo/gviz/tq?gid=', //sh
                 'https://docs.google.com/spreadsheets/d/15Bx8GtDL2xyD6OWCQNHLKG2YD98HTkGcmSb0UMIDxZ8/gviz/tq?gid='  //deimos
                 ];
+
+var bossGID = [vgGID,gorseGID,sabGID,slothGID,matthiasGID,kcGID,xeraGID,cairnGID,moGID,samGID,deimGID,shGID,dhuumGID];
+var selectedBoss = vgGID;
 var bossSelection = 0;
 var classSelection = 0;
 var GID = 0;
+var sheetLink = 'https://docs.google.com/spreadsheets/d/1L6Z8UZNQvgLi1Ve9ybmUjD6yLdDIz4D65c4FfQB3Sec/gviz/tq?gid=';
 
 // Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
+google.charts.load('current', {'packages':['corechart'], 'callback':drawChart});
+google.charts.load('current', {'packages': ['table'],'callback':drawChart});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
@@ -44,8 +229,8 @@ google.charts.setOnLoadCallback(drawChart);
 // draws it.
 function drawChart() {
   	// Call to google sheet
- 	var queryString = encodeURIComponent('SELECT B, C OFFSET 0')
-  	var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/19N3mkRwI4wrY1b84yVjbkOj3Lt-qG-xpq2iUPpYTDQU/gviz/tq?gid=" + GID +"&headers=1&tq=" + queryString);
+ 	var queryString = encodeURIComponent('')
+  	var query = new google.visualization.Query(sheetLink + GID +"&headers=1&tq=" + queryString);
   	query.send(handleQueryResponse);
 }
 
@@ -59,17 +244,21 @@ function handleQueryResponse(response) {
 
     //Create table
   	var data = response.getDataTable();
-  	var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
+  	var chart = new google.visualization.Table(document.getElementById('chart_div'));
   	chart.draw(data, null);
 }
 
 window.onload = function(){
   document.getElementById("press").onclick = function(){
     classSelection = document.getElementById("menu").value;
-    GID = cairnGID[classSelection];
+    GID = selectedBoss[classSelection];
+    bossSelection = document.getElementById("bossSelect").value;
+    selectedBoss = bossGID[bossSelection];
+    sheetLink = bossSheet[bossSelection];
     drawChart();
   };
-document.getElementById("bossPress").onclick = function(){
-    bossSelection = document.getElementById("bossSelect").value;
-  };
 };
+/*document.getElementById("bossPress").onclick = function(){
+    
+  };
+};*/
